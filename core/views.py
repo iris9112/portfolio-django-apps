3137c59128f690e14b000b1c35761a0f6fa6dd3d
iris9112 <iris9112@gmail.com>
2018-04-28 23:48:37 -0500

    remove unused import

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
-# Create your views here.
+
 def home(request):
     return render(request, "core/home.html")
 
